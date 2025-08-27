9eaa07108681215928f5a2a46cabe34b298b7d21
bharathi634593 <bharathi634593@gmail.com>
2025-08-27 14:18:15 +0530

    Added DAY-07, DAY-08, DAY-09 projects

---
+++ DAY-07/script.js/Q1.js
@@ -0,0 +1,9 @@
+// Input
+let r = parseFloat(prompt("Enter Radius of Circle:"));
+
+// Calculation
+let area = (22.0 / 7) * r * r;
+
+// Output
+console.log(`Area of Circle with Radius ${r.toFixed(4)} is ${area.toFixed(4)}`);
+alert(`Area of Circle with Radius ${r.toFixed(4)} is ${area.toFixed(4)}`);
